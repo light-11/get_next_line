6275bd8323d0a91193a7374e51bbfcb888325a8a
light-11 <violet121311@gmail.com>
2026-05-22 23:13:33 +0900

    修正中

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,11 @@
 /*   By: ayanaga <ayanaga@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/19 20:11:44 by ayanaga           #+#    #+#             */
-/*   Updated: 2026/05/20 22:57:30 by ayanaga          ###   ########.fr       */
+/*   Updated: 2026/05/22 22:51:19 by ayanaga          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-static char	*buf_insert(char *buf, ssize_t count, char *copy)
+static char	*buf_insert(char *buf, ssize_t count, char *copy, ssize_t read_file)
 {
 	int		i;
 	int		j;
@@ -18,19 +18,31 @@ static char	*buf_insert(char *buf, ssize_t count, char *copy)
 
 	i = 0;
 	j = 0;
-	copy_all = malloc(sizeof(char) * (count + BUFFER_SIZE + 1));
+	copy_all = malloc(sizeof(char) * (count + read_file + 1));
 	if (!copy_all)
+	{
+		free(copy);
 		return (NULL);
-	while (copy[i])
-		copy_all[i] = copy[i];
+	}
+	if (copy)
+	{
+		while (copy[i])
+		{
+			copy_all[i] = copy[i];
+			i++;
+		}
+	}
 	while (buf[j])
+	{
 		copy_all[i + j] = buf[j];
+		j++;
+	}
 	copy_all[i + j] = '\0';
 	free(copy);
 	return (copy_all);
 }
 
-static int	*cut_word(char *copy, ssize_t count, char *return_word)
+static int	cut_word(char *copy, char **return_word)
 {
 	int	i;
 	int	j;
@@ -41,29 +53,42 @@ static int	*cut_word(char *copy, ssize_t count, char *return_word)
 		i++;
 	if (copy[i] == '\n')
 		i++;
-	return_word = malloc(sizeof(char) * i + 1);
-	i = 0;
-	while (copy[i] != '\0' && copy[i] != '\n')
+	*return_word = malloc(sizeof(char) * i + 1);
+	if (!*return_word)
+		return (-1);
+	while (copy[j] != '\0' && copy[j] != '\n')
 	{
-		return_word[i] = copy[i];
-		i++;
+		(*return_word)[j] = copy[j];
+		j++;
 	}
-	if (copy[i] == '\n')
+	if (copy[j] == '\n')
 	{
-		i++;
-		return_word[i] = '\n';
+		(*return_word)[j] = '\n';
+		j++;
 	}
-	return (i);
+	(*return_word)[j] = '\0';
+	return (j);
 }
 
-static char	*n_copy(char *copy, ssize_t count, char *return_word)
+static char	*n_copy(char *copy, ssize_t count, char **return_word)
 {
-	int	i;
-	int	j;
+	int		i;
+	int		j;
+	char	*copy_new;
 
 	i = 0;
 	j = 0;
-	i = cut_word(copy, count, return_word);
+	i = cut_word(copy, return_word);
+	if (i == -1)
+	{
+		free(copy);
+		return (NULL);
+	}
+	if (copy[i] == '\0')
+	{
+		free(copy);
+		return (NULL);
+	}
 	copy_new = malloc(sizeof(char) * (count - i) + 1);
 	if (!copy_new)
 		return (NULL);
@@ -80,27 +105,41 @@ static char	*n_copy(char *copy, ssize_t count, char *return_word)
 
 char	*get_next_line(int fd)
 {
-	char	*buf;
-	char	*copy;
-	ssize_t	read_file;
-	ssize_t	count_all;
-	char	*return_word;
+	char		*buf;
+	static char	*copy;
+	ssize_t		read_file;
+	char		*return_word;
 
 	buf = malloc(BUFFER_SIZE + 1);
 	if (!buf)
 		return (NULL);
-	buf[BUFFER_SIZE + 1] = '\0';
 	while (1)
 	{
 		read_file = read(fd, buf, BUFFER_SIZE);
 		if (read_file < 0)
+		{
+			free(buf);
 			return (NULL);
+		}
 		if (read_file == 0)
 			break ;
-		copy = buf_insert(buf, count_all, copy);
-		count_all = read_file + count_all;
+		buf[read_file] = '\0';
+		if (copy)
+			copy = buf_insert(buf, ft_strlen(copy), copy, read_file);
+		else
+			copy = buf_insert(buf, 0, copy, read_file);
+		if (ft_strchr(copy, '\n'))
+			break ;
+	}
+	if (!copy || copy[0] == '\0')
+	{
+		free(buf);
+		free(copy);
+		copy = NULL;
+		return (NULL);
 	}
 	return_word = NULL;
-	copy = n_copy(char *copy, ssize_t count, char *return_word);
+	copy = n_copy(copy, ft_strlen(copy), &return_word);
+	free(buf);
 	return (return_word);
 }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,6 +6,34 @@
 /*   By: ayanaga <ayanaga@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/20 22:40:31 by ayanaga           #+#    #+#             */
-/*   Updated: 2026/05/20 22:40:32 by ayanaga          ###   ########.fr       */
+/*   Updated: 2026/05/22 22:06:37 by ayanaga          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stddef.h>
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if ((unsigned char)s[i] == (unsigned char)c)
+			return ((char *)&s[i]);
+		i++;
+	}
+	if (c == '\0')
+		return ((char *)&s[i]);
+	return (0);
+}
